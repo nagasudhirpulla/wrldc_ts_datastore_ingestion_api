9ec1482c2824bf0620d950023ef9aab91b017112
unknown <nagasudhirpulla@gmail.com>
2022-03-20 09:35:53 +0530

    insert record endpoint complete

--- TsDataStoreApi/Controllers/DataController.cs
+++ TsDataStoreApi/Controllers/DataController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
 using TsDataStoreApi.Infra.BasicAuth;
+using TsDataStoreApi.Infra.Persistence;
 
 namespace TsDataStoreApi.Controllers;
 [Route("[controller]")]
@@ -7,25 +9,28 @@ namespace TsDataStoreApi.Controllers;
 public class DataController : ControllerBase
 {
     private readonly ILogger<DataController> _logger;
+    private readonly ITsDataStore _tsDataStore;
 
-    public DataController(ILogger<DataController> logger)
+    public DataController(ILogger<DataController> logger, ITsDataStore tsDataStore)
     {
         _logger = logger;
+        _tsDataStore = tsDataStore;
     }
 
     [BasicAuth] // You can optionally provide a specific realm --> [BasicAuth("my-realm")]
     [HttpPost(Name = "InsertData")]
-    public Task<bool> PostAsync(DataInsCommand d)
+    public async Task<bool> PostAsync(DataInsCommand d)
     {
-        // TODO complete this
-        // datetime string will be in the format of yyyy-MM-dd HH:mm:ss
-        return Task.FromResult(true);
+        // datetime should be in yyyy-MM-dd HH:mm:ss format
+        var ts = DateTime.ParseExact(d.Ts ?? "", "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture);
+        bool isOk = await _tsDataStore.Insert(ts, d.MeasId ?? "", d.Val);
+        return isOk;
     }
 
     public class DataInsCommand
     {
-        public string Ts { get; set; }
-        public string MeasId { get; set; }
+        public string? Ts { get; set; }
+        public string? MeasId { get; set; }
         public double Val { get; set; }
 
     }
